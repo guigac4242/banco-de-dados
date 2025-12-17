de325629ef3dfd8f3f6d83fc30f2d96c0786e663
Guilherme <163591623+guigac4242@users.noreply.github.com>
2025-12-17 10:24:50 -0300

    banco-dados-relacional-sql
    📁 banco-dados-relacional-sql
     ├── schema.sql
     ├── dados.sql
     ├── consultas.sql
     └── README.md

--- banco de dados.sql
+++ banco de dados.sql
@@ -15,4 +15,28 @@ CREATE TABLE venda (
     valor DECIMAL(10,2),
     PRIMARY KEY (id_cliente, id_produto, data_venda),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-   
+    FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
+);
+
+INSERT INTO produto VALUES ('P01','Carro');
+INSERT INTO produto VALUES ('P02','Moto');
+INSERT INTO produto VALUES ('P03','Bicicleta');
+
+INSERT INTO cliente VALUES ('C01','João Pedro');
+INSERT INTO cliente VALUES ('C02','Maria José');
+INSERT INTO cliente VALUES ('C03','Carla');
+
+INSERT INTO venda VALUES ('C01','P02','2023-10-10',5000.00);
+INSERT INTO venda VALUES ('C01','P02','2023-10-15',3000.00);
+INSERT INTO venda VALUES ('C01','P01','2023-10-10',6000.00);
+INSERT INTO venda VALUES ('C03','P01','2023-09-21',4500.00);
+INSERT INTO venda VALUES ('C03','P02','2023-10-10',2300.00);
+
+SELECT
+    c.nome_cliente,
+    p.nome_produto,
+    v.data_venda,
+    v.valor
+FROM venda v
+JOIN cliente c ON v.id_cliente = c.id_cliente
+JOIN produto p ON v.id_produto = p.id_produto;
